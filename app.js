99059d00a35078ef60c1fde2ce2b9ed8b6ed6d61
Barri <info@bdesignstudio.co.uk>
2020-11-03 17:41:49 +0000

    Make a class and constructors and start function

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-console.log('ll');
+class Timer{
+  constructor(durationInput, startButton, pauseButton ){
+    this.durationInput = durationInput;
+    this.startButton = startButton;
+    this.pauseButton = pauseButton;
+    
+    //to make sure start runs each time we click the start button we make an event listener here 
+    this.startButton.addEventListener('click', this.start())
+  }
+  start(){
+    console.log('Time to start the timer');
+  }
+}
