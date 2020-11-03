8c5e0548c3ba5034376c69f5bdbac01c4460e49f
Barri <info@bdesignstudio.co.uk>
2020-11-03 17:53:01 +0000

    bind event in a class

--- app.js
+++ app.js
@@ -3,11 +3,16 @@ class Timer{
     this.durationInput = durationInput;
     this.startButton = startButton;
     this.pauseButton = pauseButton;
-    
+
     //to make sure start runs each time we click the start button we make an event listener here 
-    this.startButton.addEventListener('click', this.start())
+    this.startButton.addEventListener('click', this.start)
   }
   start(){
     console.log('Time to start the timer');
   }
 }
+
+const durationInput= document.querySelector('#duration');
+const startButton = document.querySelector('#start');
+const pauseButton = document.querySelector('#pause');
+const timer = new Timer(durationInput, startButton, pauseButton);
