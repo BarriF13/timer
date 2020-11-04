ed4710b0493298eaf8af1d2d2fce12badcd5c078
Barri <info@bdesignstudio.co.uk>
2020-11-04 17:51:28 +0000

    separade the timer class

--- app.js
+++ app.js
@@ -23,66 +23,66 @@
 // }
 
 
-class Timer {
-  constructor(durationInput, startButton, pauseButton, callbacks) {
-    this.durationInput = durationInput;
-    this.startButton = startButton;
-    this.pauseButton = pauseButton;
-    if(callbacks){
-      this.onStart = callbacks.onStart;
-      this.onTick = callbacks.onTick;
-      this.onComplete = callbacks.onComplete;
-    }
-    //to make sure start runs each time we click the start button we make an event listener here 
-    this.startButton.addEventListener('click', this.start);
-    this.pauseButton.addEventListener('click', this.pause);
-  }
+// class Timer {
+//   constructor(durationInput, startButton, pauseButton, callbacks) {
+//     this.durationInput = durationInput;
+//     this.startButton = startButton;
+//     this.pauseButton = pauseButton;
+//     if(callbacks){
+//       this.onStart = callbacks.onStart;
+//       this.onTick = callbacks.onTick;
+//       this.onComplete = callbacks.onComplete;
+//     }
+//     //to make sure start runs each time we click the start button we make an event listener here 
+//     this.startButton.addEventListener('click', this.start);
+//     this.pauseButton.addEventListener('click', this.pause);
+//   }
 
-  start = () => {
-    if(this.onStart){
-      this.onStart();
-    }
-    this.tick();
-    this.interval = setInterval(this.tick, 1000);// setInterval run function in interval and save it in timer so we can pause it below-- then we change const timer to this.timer to access it on the pause function 
-    //clearInterval(timer);
-  };
-  pause = () => {
-    clearInterval(this.interval);
+//   start = () => {
+//     if(this.onStart){
+//       this.onStart();
+//     }
+//     this.tick();
+//     this.interval = setInterval(this.tick, 1000);// setInterval run function in interval and save it in timer so we can pause it below-- then we change const timer to this.timer to access it on the pause function 
+//     //clearInterval(timer);
+//   };
+//   pause = () => {
+//     clearInterval(this.interval);
 
-  }
-  tick = () => {
-    //const timeRemaining = parseFloat(this.durationInput.value);
-    //Using get method we can point it faster
-    //const timeRemaining = this.timeRemaining;
-    if (this.timeRemaining <= 0) {
-       this.pause();
-      if(this.onComplete){
-        this.onComplete();
-      }
-    } else {
+//   }
+//   tick = () => {
+//     //const timeRemaining = parseFloat(this.durationInput.value);
+//     //Using get method we can point it faster
+//     //const timeRemaining = this.timeRemaining;
+//     if (this.timeRemaining <= 0) {
+//        this.pause();
+//       if(this.onComplete){
+//         this.onComplete();
+//       }
+//     } else {
 
-      this.timeRemaining = this.timeRemaining - 1;
-      if(this.onTick){
-        this.onTick();
-      }
-    }
+//       this.timeRemaining = this.timeRemaining - 1;
+//       if(this.onTick){
+//         this.onTick();
+//       }
+//     }
 
-  };
-  //get = instance var maker
-  get timeRemaining() {
-    return parseFloat(this.durationInput.value);
-  }
-  set timeRemaining(time) {
-    this.durationInput.value = time;
-  }
-  //helper methods
-  // getTime() {
-  //   return  parseFloat(this.durationInput.value);
-  // }
-  // setTime(time) {
-  //   this.durationInput.value = time;
-  // }
-}
+//   };
+//   //get = instance var maker
+//   get timeRemaining() {
+//     return parseFloat(this.durationInput.value);
+//   }
+//   set timeRemaining(time) {
+//     this.durationInput.value = time;
+//   }
+//   //helper methods
+//   // getTime() {
+//   //   return  parseFloat(this.durationInput.value);
+//   // }
+//   // setTime(time) {
+//   //   this.durationInput.value = time;
+//   // }
+// }
 const durationInput = document.querySelector('#duration');
 const startButton = document.querySelector('#start');
 const pauseButton = document.querySelector('#pause');
@@ -98,4 +98,3 @@ const timer = new Timer(durationInput, startButton, pauseButton, {
     console.log('Timer is completed');
   }
 });
-//timer.start();
