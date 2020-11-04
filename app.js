18d777a9dcbec206f1aab035b91200b5c546b785
Barri <info@bdesignstudio.co.uk>
2020-11-04 13:12:17 +0000

    Set up an interval for timer

--- app.js
+++ app.js
@@ -1,3 +1,28 @@
+//*********** Learning this on codes below ***************/
+// class Timer{
+//   constructor(durationInput, startButton, pauseButton ){
+//     this.durationInput = durationInput;
+//     this.startButton = startButton;
+//     this.pauseButton = pauseButton;
+   
+//     //to make sure start runs each time we click the start button we make an event listener here 
+//     //this.startButton.addEventListener('click', this.start);
+//     this.startButton.addEventListener('click', this.startBind.bind(this))
+//   }
+//   startBind(){
+//     console.log('I am using bind');
+//   }
+//   start = ()=>{
+//    // console.log(this);
+//     console.log('Time to start the timer');
+//     this.importantMeth();
+//   }
+//   importantMeth(){
+//     console.log('bla');
+//   }
+// }
+
+
 class Timer{
   constructor(durationInput, startButton, pauseButton ){
     this.durationInput = durationInput;
@@ -5,20 +30,19 @@ class Timer{
     this.pauseButton = pauseButton;
    
     //to make sure start runs each time we click the start button we make an event listener here 
-    //this.startButton.addEventListener('click', this.start);
-    this.startButton.addEventListener('click', this.startBind.bind(this))
-  }
-  startBind(){
-    console.log('I am using bind');
-  }
+    this.startButton.addEventListener('click', this.start);
+     }
+ 
   start = ()=>{
-   // console.log(this);
-    console.log('Time to start the timer');
-    this.importantMeth();
-  }
-  importantMeth(){
-    console.log('bla');
-  }
+    this.tick();
+    const timer =setInterval(this.tick, 1000);// setInterval run function in interval and save it in timer so we can pause it below 
+    clearInterval(timer);
+  };
+
+  tick=()=>{
+    console.log('tick');
+  };
+
 }
 
 const durationInput= document.querySelector('#duration');
